df760c445720257690c8f41e78e7cde41239d1c3
YuriSolovyov <biohazard707@gmail.com>
2015-11-11 22:56:45 +0300

    Open a page

---
+++ background.js
@@ -0,0 +1,20 @@
+(function(self) {
+    'use strict';
+    const optionsUrl = chrome.extension.getURL('index.html');
+
+    const openExtPage = function() {
+        chrome.tabs.query({ url: optionsUrl }, function(tabs) {
+            if (tabs.length) {
+                chrome.tabs.update(tabs[0].id, { active: true });
+            } else {
+                chrome.tabs.create({ url: optionsUrl });
+            }
+        });
+    };
+
+    chrome.commands.onCommand.addListener(function(command) {
+       console.log('Command:', command);
+       openExtPage();
+    });
+
+})(this);
