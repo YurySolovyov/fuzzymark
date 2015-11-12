15a96c9c761d0f118b06736684640954708e4aa8
YuriSolovyov <biohazard707@gmail.com>
2015-11-12 21:55:39 +0300

    Implement actual selecting and opening of bookmarks. Yay!

--- background.js
+++ background.js
@@ -2,18 +2,24 @@
     'use strict';
     const optionsUrl = chrome.extension.getURL('index.html');
 
-    const openExtPage = function() {
-        chrome.tabs.query({ url: optionsUrl }, function(tabs) {
+    const createOrSelectTab = function(url) {
+        chrome.tabs.query({ url: url }, function(tabs) {
             if (tabs.length) {
                 chrome.tabs.update(tabs[0].id, { active: true });
             } else {
-                chrome.tabs.create({ url: optionsUrl });
+                chrome.tabs.create({ url: url });
             }
         });
     };
 
     chrome.commands.onCommand.addListener(function(command) {
-       openExtPage();
+       createOrSelectTab(optionsUrl);
+    });
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.type === 'open_tab') {
+            createOrSelectTab(request.url);
+        }
     });
 
 })(this);
--- main.js
+++ main.js
@@ -4,7 +4,12 @@ $(function() {
     const results = $('#results');
     const input = $('#input');
 
+    const upKey = 38;
+    const downKey = 40;
+    const enterKey = 13;
+
     const store = new Map();
+    const keyHandlers = new Map();
 
     const getRawBookmarks = function() {
         return new Promise(function(resolve) {
@@ -26,7 +31,8 @@ $(function() {
         results.empty();
 
         const elements = matched.slice(0, 20).map(function(item, index) {
-            const root = $('<li class="bookmark" />');
+            const rootClasses = index === 0 ? 'bookmark selected' : 'bookmark';
+            const root = $('<li />').addClass(rootClasses);
             const title = $('<span class="bookmarkTitle" />').text(item.title || item.url);
             const url = $('<span class="bookmarkUrl" />').text(item.url);
 
@@ -36,6 +42,30 @@ $(function() {
         results.append(elements);
     };
 
+    const selectNext = function() {
+        let next = results.find('.selected').removeClass('selected').next();
+        if (next.length === 0) {
+            next = results.find('.selected').first();
+        }
+        next.addClass('selected');
+    };
+
+    const selectPrev = function() {
+        let prev = results.find('.selected').removeClass('selected').prev();
+        if (prev.length === 0) {
+            prev = results.find('.selected').last();
+        }
+        prev.addClass('selected');
+    };
+
+    const openSelected = function() {
+        const url = results.find('.selected .bookmarkUrl').text();
+        chrome.runtime.sendMessage({
+            type: 'open_tab',
+            url: url
+        });
+    };
+
     getRawBookmarks().then(flattenBookmarks).then(function(bookmarks) {
         store.set('bookmarks', bookmarks);
     });
@@ -43,7 +73,15 @@ $(function() {
     input.on('input', function(e) {
         store.set('value', e.target.value);
         render();
+    }).on('keydown', function(e) {
+        if (keyHandlers.has(e.keyCode)) {
+            keyHandlers.get(e.keyCode)();
+        }
     });
+
+    keyHandlers.set(upKey, selectPrev);
+    keyHandlers.set(downKey, selectNext);
+    keyHandlers.set(enterKey, openSelected);
 
     $('#input').focus();
 });
