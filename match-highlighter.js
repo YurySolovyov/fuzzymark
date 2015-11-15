92d61498060769d7ce580d62cf54181140be0dea
YuriSolovyov <biohazard707@gmail.com>
2015-11-15 23:36:44 +0300

    Merge merged matched ranges to produce continious string. Fixes #8

--- match-highlighter.js
+++ match-highlighter.js
@@ -1,20 +1,40 @@
 function MatchHighlighter() {
     'use strict';
 
+    const mergeRanges = function(indexes) {
+        return indexes.reduce(function(obj, index, pos) {
+            const prevIndex = indexes[pos - 1] || 0;
+            const currentIndex = indexes[pos];
+            const nextIndex = indexes[pos + 1] || 0;
+
+            if (currentIndex === nextIndex - 1 || currentIndex === prevIndex + 1) {
+                obj.ranges[obj.rangeIndex].push(currentIndex);
+            } else {
+                obj.rangeIndex = obj.ranges.push([currentIndex]);
+                obj.ranges.push([]);
+            }
+            return obj;
+        }, {
+            ranges:[[]],
+            rangeIndex: 0
+        }).ranges;
+    };
+
     const wrapHighlight = function(letter) {
         return '<b>' + letter + '</b>';
     };
 
     this.highlight = function(input, result) {
         const matched = FuzzaldrinPlus.match(result, input);
-
-        const letters = result.split('');
-
-        matched.forEach(function(letterIndex) {
-            letters[letterIndex] = wrapHighlight(letters[letterIndex]);
+        const substrings = mergeRanges(matched).map(function(range) {
+            return range.map(function(index) {
+                return result.charAt(index);
+            }).join('');
         });
 
-        return letters.join('');
+        return substrings.reduce(function(res, substring) {
+            return res.replace(substring, wrapHighlight(substring));
+        }, result);
     };
 
     return this;
