fd259ade26f9caabae0cfc5fa7f1a9c30077e44a
YuriSolovyov <biohazard707@gmail.com>
2015-11-17 23:13:48 +0300

    Rewrite range reducer. Add tests

--- src/frontend/match-highlighter.js
+++ src/frontend/match-highlighter.js
@@ -1,23 +1,7 @@
 function MatchHighlighter() {
     'use strict';
 
-    const mergeRanges = function(indexes) {
-        return indexes.reduce(function(obj, index, pos) {
-            const prevIndex = indexes[pos - 1] || 0;
-            const currentIndex = indexes[pos];
-            const nextIndex = indexes[pos + 1] || 0;
-
-            if (currentIndex === nextIndex - 1 || currentIndex === prevIndex + 1 || indexes.length === 1) {
-                obj.ranges[obj.rangeIndex].push(currentIndex);
-            } else if (currentIndex > 0 && indexes.length > 1) {
-                obj.rangeIndex = obj.ranges.push([currentIndex]) - 1;
-            }
-            return obj;
-        }, {
-            ranges:[[]],
-            rangeIndex: 0
-        }).ranges;
-    };
+    const reducer = new RangesReducer();
 
     const wrapHighlight = function(letter) {
         return '<b>' + letter + '</b>';
@@ -25,7 +9,7 @@ function MatchHighlighter() {
 
     this.highlight = function(input, result) {
         const matched = FuzzaldrinPlus.match(result, input);
-        const substrings = mergeRanges(matched).map(function(range) {
+        const substrings = reducer.reduce(matched).map(function(range) {
             return range.map(function(index) {
                 return result.charAt(index);
             }).join('');
---
+++ src/frontend/ranges-reducer.js
@@ -0,0 +1,28 @@
+function RangesReducer() {
+    'use strict';
+
+    this.reduce = function(indexes) {
+        return indexes.reduce(function(ranges, index, pos) {
+            const prevIndex = indexes[pos - 1];
+            const currentIndex = indexes[pos];
+            const nextIndex = indexes[pos + 1];
+
+            const rangeBehind = currentIndex === prevIndex + 1;
+            const rangeAhead = currentIndex === nextIndex - 1 && rangeBehind;
+
+            const currentSlot = ranges.length - 1;
+
+            let slot;
+            if (rangeAhead || rangeBehind) {
+                slot = ranges[currentSlot];
+            } else {
+                slot = [];
+                ranges.push(slot);
+            }
+
+            slot.push(currentIndex);
+
+            return ranges;
+        }, []);
+    };
+}
