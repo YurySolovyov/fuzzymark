45dd2e84204ed9742ae72beef3a0b12352ef5355
Yury Solovyov <biohazard707@gmail.com>
2017-08-12 20:54:48 +0300

    kebab-case

--- src/frontend/components/SearchItem.vue
+++ src/frontend/components/SearchItem.vue
@@ -1,15 +1,15 @@
 <template lang="html">
   <li class="bookmark overflow-hidden border-box list-style-none"
     :class="{ selected: bookmark.selected }">
-    <div class="bookmarkWrapper p1 container-background">
-      <div class="bookmarkHeader p1 overflow-hidden nowrap">
-        <span class="bookmarkScore">{{ bookmark.score }}</span>
-        <span class="bookmarkTitle" v-html="bookmark.wrappedTitle"></span>
-        <span class="bookmarkPath right">{{ bookmark.path }}</span>
+    <div class="bookmark-wrapper p1 container-background">
+      <div class="bookmark-header p1 overflow-hidden nowrap">
+        <span class="bookmark-score">{{ bookmark.score }}</span>
+        <span class="bookmark-title" v-html="bookmark.wrappedTitle"></span>
+        <span class="bookmark-path right">{{ bookmark.path }}</span>
       </div>
-      <div class="bookmarkFooter p1 overflow-hidden nowrap">
-        <img class="bookmarkFavicon inline-block" :src="bookmark.favicon" />
-        <a :href="bookmark.url" class="bookmarkUrl" v-html="bookmark.wrappedUrl"></a>
+      <div class="bookmark-footer p1 overflow-hidden nowrap">
+        <img class="bookmark-favicon inline-block" :src="bookmark.favicon" />
+        <a :href="bookmark.url" class="bookmark-url" v-html="bookmark.wrappedUrl"></a>
       </div>
     </div>
   </li>
@@ -34,51 +34,51 @@ export default {
   background-image: linear-gradient(to right, var(--theme-selected-start) 0%,var(--theme-selected-end) 100%);
 }
 
-.bookmark.selected .bookmarkWrapper {
+.bookmark.selected .bookmark-wrapper {
   background-color: var(--theme-selected-bg-color);
 }
 
-.bookmarkWrapper {
+.bookmark-wrapper {
   transition: 0.3s;
 }
 
-.bookmarkHeader, .bookmarkFooter {
+.bookmark-header, .bookmark-footer {
   font-size: 16px;
   text-overflow: ellipsis;
 }
 
-.bookmarkHeader {
+.bookmark-header {
   color: var(--theme-header-color);
 }
 
-.bookmarkHeader b {
+.bookmark-header b {
   color: var(--theme-selected-start);
 }
 
-.bookmarkFooter {
+.bookmark-footer {
   border-top: 1px var(--theme-bookmark-divider-color) solid;
 }
 
-.bookmarkPath {
+.bookmark-path {
   color: var(--theme-bookmark-link-color);
   font-size: 14px;
 }
 
-.bookmarkScore {
+.bookmark-score {
   color: var(--theme-selected-start);
   margin-right: 16px;
 }
 
-.bookmarkUrl {
+.bookmark-url {
   color: var(--theme-bookmark-link-color);
 }
 
-.bookmarkUrl:hover {
+.bookmark-url:hover {
   color: var(--theme-bookmark-link-hover-color);
   text-decoration: underline;
 }
 
-.bookmarkFavicon {
+.bookmark-favicon {
   width: 16px;
   height: 16px;
   margin: -4px 4px 0 -4px;
