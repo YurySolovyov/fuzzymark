1b71142740a326c6889f92bc97e6954a3296aeda
Yury Solovyov <biohazard707@gmail.com>
2017-08-12 21:11:34 +0300

    Minor tweaks

--- src/frontend/components/TileControls.vue
+++ src/frontend/components/TileControls.vue
@@ -1,18 +1,18 @@
 <template lang="html">
-  <div class="tile-controls right" @click.prevent.stop>
+  <div class="tile-controls right" @click.prevent.stop @mouseleave="collapse">
     <div class="dots" @click="toggle">
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="#6F6F6F" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
+        <path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
       </svg>
     </div>
     <div class="edit" v-if="expanded" @click="onEdit" title="Edit Tile">
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="#6F6F6F" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+        <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
       </svg>
     </div>
     <div class="delete" v-if="expanded" @click="onDelete" title="Delete Tile">
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="#6F6F6F" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+        <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
       </svg>
     </div>
   </div>
@@ -26,6 +26,9 @@ export default {
     };
   },
   methods: {
+    collapse() {
+      this.expanded = false;
+    },
     toggle() {
       this.expanded = !this.expanded;
     },
@@ -50,6 +53,10 @@ export default {
   transition: 0.3s;
   opacity: 0;
   cursor: pointer;
+}
+
+.tile-controls path {
+  fill: var(--input-text-color);
 }
 
 </style>
