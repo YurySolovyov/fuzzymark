b3b6d17e9102e970d860d35062b4bb53c61e1f5d
Yury Solovyov <biohazard707@gmail.com>
2017-08-16 22:01:36 +0300

    Fix leftiver command handler

--- src/backend/background/background.js
+++ src/backend/background/background.js
@@ -7,7 +7,6 @@ const handleOnInstalled = require('./install-handler.js');
 const indexUrl = chrome.runtime.getURL('static/index.html');
 const openOptions = createOrSelectTab.bind(null, indexUrl);
 
-chrome.commands.onCommand.addListener(openOptions);
 chrome.browserAction.onClicked.addListener(openOptions);
 chrome.runtime.onMessage.addListener(processStoreMessages);
 chrome.runtime.onInstalled.addListener(handleOnInstalled);
