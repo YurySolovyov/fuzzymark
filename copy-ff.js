49a6eeec79d2cf32c277009066d2752a21bb0dfb
Yury Solovyov <invokedynamic@yandex.com>
2023-09-15 22:38:59 +0500

    make it run in firefox

--- copy-ff.js
+++ copy-ff.js
@@ -12,10 +12,25 @@ const copy = async () => {
 };
 
 const fixup = async () => {
-  const manifestString = await readFile(path.join(CHROME_DIR, 'manifest.json'), 'utf-8');
-  const { browser_specific_settings: _drop, ...manifestUpdated } = JSON.parse(manifestString);
-  
-  await writeFile(path.join(CHROME_DIR, 'manifest.json'), JSON.stringify(manifestUpdated, null, 2));
+  // Chrome does not like extra browser_specific_settings prop
+  {
+    const manifestString = await readFile(path.join(CHROME_DIR, 'manifest.json'), 'utf-8');
+    const { browser_specific_settings: _drop, ...manifestUpdated } = JSON.parse(manifestString);
+
+    await writeFile(path.join(CHROME_DIR, 'manifest.json'), JSON.stringify(manifestUpdated, null, 2));
+  }
+
+  // Firefox can't do service workers in extensions, but can do background scripts
+  {
+    const manifestString = await readFile(path.join(FIREFOX_DIR, 'manifest.json'), 'utf-8');
+    const { background, ...manifestUpdated } = JSON.parse(manifestString);
+
+    manifestUpdated.background = {
+      scripts: [background.service_worker]
+    };
+
+    await writeFile(path.join(FIREFOX_DIR, 'manifest.json'), JSON.stringify(manifestUpdated, null, 2));
+  }
 }
 
 await copy();
